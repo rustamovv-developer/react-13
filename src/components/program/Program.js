393eb8a72a286fb56074a3ac15fcf870b56a6341
--global <--global>
2024-04-09 22:29:47 +0300

    finished

---
+++ src/components/program/Program.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Program extends Component {
+  render() {
+    return (
+      <section className="program">
+        <div className="container"></div>
+      </section>
+    );
+  }
+}
